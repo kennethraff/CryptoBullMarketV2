9c98c9687cb8858cc54dc3265d07f000d253e477
tegarmarino <tmarino01@student.ciputra.ac.id>
2023-05-26 12:50:22 +0700

    Initial Commit

---
+++ CryptoTracker/CryptoTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoTrackerApp.swift
+//  CryptoTracker
+//
+//  Created by Tegar Marino on 26/05/23.
+//
+
+import SwiftUI
+
+@main
+struct CryptoTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
